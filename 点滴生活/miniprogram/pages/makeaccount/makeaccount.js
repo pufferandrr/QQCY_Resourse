f7382d54d79f5540cbcf300bdbf7612524c7b051
csk <1649537125@qq.com>
2021-04-27 23:24:40 +0800

    创建账单页面 新增简单展示类型

--- 点滴生活/miniprogram/pages/makeaccount/makeaccount.js
+++ 点滴生活/miniprogram/pages/makeaccount/makeaccount.js
@@ -1,18 +1,37 @@
 // miniprogram/pages/makeaccount/makeaccount.js
+const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    nvabarData: {
+      showCapsule: 1, //是否显示左上角返回图标   1表示显示    0表示不显示
+      showEdit:0,//是否显示左上角编辑图标   1表示显示    0表示不显示
+      showcancel:0,//是否显示左上角关闭图标   1表示显示    0表示不显示
+      title: '记账', //导航栏 中间的标题
+    },
+    height: app.globalData.height * 2 + 20 , // 此页面 页面内容距最顶部的距离
+    type:[],
   },
-
+  returnA:function(){
+    wx.navigateBack();
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    wx.cloud.callFunction({
+      name:"getType",
+      data:{
+        "type": 1,
+      }
+    }).then(res=>{
+      this.setData({
+        type:res.result.data
+      })
+    })
   },
 
   /**
