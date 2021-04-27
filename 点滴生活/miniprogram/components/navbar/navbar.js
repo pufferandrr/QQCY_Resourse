5906efd6990c3122a1986d7d31c5087e735c9347
hzj2870757140 <https://github.com/hzj2870757140>
2021-04-27 22:59:43 +0800

    稍微完成了点账单列表
    还有一些样式的改进，并且数据还没有动态获取

--- 点滴生活/miniprogram/components/navbar/navbar.js
+++ 点滴生活/miniprogram/components/navbar/navbar.js
@@ -3,9 +3,10 @@ Component({
   properties: {
     navbarData: {   //navbarData   由父页面传递的数据，变量名字自命名
       type: Object,
-      value: {},
+      value: {
+      },
       observer: function (newVal, oldVal) {}
-    }
+    },
   },
   data: {
     height: '',
@@ -14,13 +15,9 @@ Component({
       showCapsule: 1,
       showEdit:1,
       showcancel:1,
-    }
+    },
   },
   attached: function () {
-    // 获取是否是通过分享进入的小程序
-    this.setData({
-      share: app.globalData.share
-    })
     // 定义导航栏的高度   方便对齐
     this.setData({
       height: app.globalData.height
@@ -29,8 +26,17 @@ Component({
   methods: {
     // 返回上一页面
     _navback(event) {
-      // 当前页面为“成长记录”时，对返回按钮进行劫持，直接跳转到首页\
-      wx.navigateBack()
+      console.log("you click back");
+      this.triggerEvent('navback');
     },
+    _navedit(event){
+      console.log("you click edit ");
+      //跳转的函数
+      this.triggerEvent('navedit');
+    },
+    _navcalcel(event){
+      console.log("you click cancel");
+      this.triggerEvent('navcancel')
+    }
   }
 })
