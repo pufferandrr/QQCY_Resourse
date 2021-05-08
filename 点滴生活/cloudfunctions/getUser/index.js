73de9685f4f69cf4685cd8dca90c964eb60fdc44
koopi0123 <1034088383@qq.com>
2021-05-08 20:43:45 +0800

    主页的名片基本信息显示

---
+++ 点滴生活/cloudfunctions/getUser/index.js
@@ -0,0 +1,32 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+
+// 根据用户openid查询数据库，返回用户id等基本信息
+cloud.init()
+const db = cloud.database();
+// 云函数入口函数
+exports.main = async (event, context) => {
+  const wxContext = cloud.getWXContext()
+  var openid = wxContext.OPENID
+  var rest
+  var sum
+  await db.collection('user')
+  .where({
+    userid:'test01'  //测试用，后面test01要改成openid
+  })
+  .get()
+  .then(res=>{
+    rest = res.data[0]
+  })
+  await db.collection('rRecord')
+  .where({
+    userid:'oxmoK5sBbtkc-ijLn6R2pkbcvSs4'  //测试用，后面userid的值要改成openid
+  })
+  .count()
+  .then(res=>{
+    sum = res.total
+  })
+  rest.accounts = sum
+  return rest
+}
--- 点滴生活/miniprogram/pages/mycenter/mycenter.js
+++ 点滴生活/miniprogram/pages/mycenter/mycenter.js
@@ -15,10 +15,16 @@ Page({
       avatorUrl:"",  //用户头像
     },
     height: app.globalData.height * 2 + 20 , // 此页面 页面内容距最顶部的距离
+    // 用户基本数据
+    userData:{
+      continuous:'-', //连续记账天数
+      total:'-', //记账总天数
+      accounts:'-', //记账总笔数
+    },
   },
 
   onLoad(option){
-    console.log(option)
+    
   },
   toNotification:function(){
     wx.navigateTo({
@@ -33,9 +39,6 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (e) {
-   
-  },
   swInput: function (e) {
 
     this.setData({
@@ -68,7 +71,22 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
+    var userInfo 
 
+    //目前阶段getUser云函数目前只搜索openid为test01的用户
+    wx.cloud.callFunction({
+      name: 'getUser',
+    }).then(res=>{
+      userInfo = res.result
+      console.log(userInfo)
+      console.log(userInfo.accounts)
+      this.setData({
+        'userData.total': userInfo.userTotalDays,
+        'userData.continuous': userInfo.userDuration,
+        'userData.accounts': userInfo.accounts
+      })
+    })
+    
   },
 
   /**
