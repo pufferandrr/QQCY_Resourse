e749a9d283fa71c2091f1a6d1617662d4065dd92
hb594709591 <594709591@qq.com>
2021-05-12 12:53:01 +0800

    将额度条的更改放进生命周期的show中这样每次更改都会随着变化

--- 点滴生活/miniprogram/components/hb/limitprogress/limitprogress.js
+++ 点滴生活/miniprogram/components/hb/limitprogress/limitprogress.js
@@ -23,45 +23,48 @@ Component({
     isSi: true,
     remain: 0,
   },
-  created: function (options) {
-    wx.cloud.callFunction({
-      name:'getLimit',
-    }).then(res=>{
-      if((res.result[1]/res.result[0])*100 <=50){
-        this.setData({
+  pageLifetimes: {
+    // 组件所在页面的生命周期函数
+    show: function () {
+      wx.cloud.callFunction({
+        name:'getLimit',
+      }).then(res=>{
+        if((res.result[1]/res.result[0])*100 <=50){
+          this.setData({
+            percent : (res.result[1]/res.result[0])*100,
+            remain :(res.result[0]-res.result[1]).toFixed(2),
+            progresscolor : "#33FFCC",
+          })
+        }
+        else if((res.result[1]/res.result[0])*100<=75)
+        {
+          this.setData({
           percent : (res.result[1]/res.result[0])*100,
           remain :(res.result[0]-res.result[1]).toFixed(2),
-          progresscolor : "#33FFCC",
+          progresscolor : "#FFC8A1",
+          noticetext : "额度已经过半，注意节约使用~"
         })
-      }
-      else if((res.result[1]/res.result[0])*100<=75)
-      {
-        this.setData({
-        percent : (res.result[1]/res.result[0])*100,
-        remain :(res.result[0]-res.result[1]).toFixed(2),
-        progresscolor : "#FFC8A1",
-        noticetext : "额度已经过半，注意节约使用~"
+        }
+        else if((res.result[1]/res.result[0])*100<100)
+        {
+          this.setData({
+            percent : (res.result[1]/res.result[0])*100,
+            remain :(res.result[0]-res.result[1]).toFixed(2),
+            progresscolor : "#F58B7E",
+            noticetext : "额度即将用完，请规划使用剩下额度~"
+          })
+        }
+        else{
+          this.setData({
+            percent : (res.result[1]/res.result[0])*100,
+            remain :(res.result[0]-res.result[1]).toFixed(2),
+            progresscolor : "#FF0000",
+            noticetext : "额度已经用完！"
+          })
+        }
+        
       })
-      }
-      else if((res.result[1]/res.result[0])*100<100)
-      {
-        this.setData({
-          percent : (res.result[1]/res.result[0])*100,
-          remain :(res.result[0]-res.result[1]).toFixed(2),
-          progresscolor : "#F58B7E",
-          noticetext : "额度即将用完，请规划使用剩下额度~"
-        })
-      }
-      else{
-        this.setData({
-          percent : (res.result[1]/res.result[0])*100,
-          remain :(res.result[0]-res.result[1]).toFixed(2),
-          progresscolor : "#FF0000",
-          noticetext : "额度已经用完！"
-        })
-      }
-      
-    })
+     },
   },
  
   /**
