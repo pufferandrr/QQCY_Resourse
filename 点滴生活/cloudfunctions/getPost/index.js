ecd2b739ee21d42e49aec2c75547a709e5a51f88
koopi0123 <1034088383@qq.com>
2021-05-13 14:01:06 +0800

    正确显示未入选列表

--- 点滴生活/cloudfunctions/getPost/index.js
+++ 点滴生活/cloudfunctions/getPost/index.js
@@ -3,14 +3,27 @@ const cloud = require('wx-server-sdk')
 cloud.init()
 
 const db = cloud.database();
+const _ = db.command
 // 云函数入口函数
 exports.main = async (event, context) => {
   const wxContext = cloud.getWXContext()
   var openid = 'test01'
   var chosen 
-  //state为1表示文章已入选, 0表示未入选
+  //state为1表示文章已入选, 0， 2表示未入选
   var state = event.state
   //var openid = wxContext.OPENID
+  if(state==='2'||state==='0'){
+    await db.collection('post')
+    .where({
+      state:_.or(_.eq('0'), _.eq('2')),
+      userid:openid,
+    })
+    .get()
+    .then(res=>{
+      chosen = res.data
+    })
+    return chosen
+  }
   await db.collection('post')
   .where({
     state:state,  
