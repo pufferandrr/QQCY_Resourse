9589364b6ae436c58c82e2fe37b7861d1631d77c
pufferandrr <49437243+pufferandrr@users.noreply.github.com>
2021-06-05 10:42:58 +0800

    创建小贴士详情界面

---
+++ 点滴生活/miniprogram/pages/tippage.js
@@ -0,0 +1,74 @@
+// pages/tippage.js
+const app = getApp()
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    nvabarData: {
+      showCapsule: 1, //是否显示左上角返回图标   1表示显示    0表示不显示
+      showEdit:0,//是否显示左上角编辑图标   1表示显示    0表示不显示
+      showcancel:0,//是否显示左上角关闭图标   1表示显示    0表示不显示
+      title: '贴士详情', //导航栏 中间的标题
+    },
+    height: app.globalData.height * 2 + 20 , // 此页面 页面内容距最顶部的距离
+    headSrc:"../images/tt3x.png"
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
